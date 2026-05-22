7946f8bcbc48d8062ff6fc7a7c2b7eba607f78e0
Tiago C Cardoso <t.iago.si.ti@gmail.com>
2026-05-22 17:28:47 -0300

    Fix admin login page centering and responsive layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import SideNav from '../components/layout/SideNav'
 
 export default function DashboardLayout({
@@ -5,16 +8,19 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+  const isAuthScreen = pathname === '/admin'
+
+  if (isAuthScreen) {
+    return <>{children}</>
+  }
+
   return (
-    <>
-      <div className='flex min-h-screen flex-col md:flex-row md:overflow-hidden'>
-        <div className='w-full flex-none md:w-64'>
-          <SideNav />
-        </div>
-        <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
-          {children}
-        </div>
+    <div className='flex min-h-screen flex-col md:flex-row md:overflow-hidden'>
+      <div className='w-full flex-none md:w-64'>
+        <SideNav />
       </div>
-    </>
+      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+    </div>
   )
 }
